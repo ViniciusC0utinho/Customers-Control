a57fe3552140953194c30699f22f2448646ac918
Vinicius Coutinho <viniciusdev@icloud.com>
2025-02-23 23:14:13 -0300

    :feat Begin Project

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    @vite('resources/js/app.js')
+</body>
+</html>
